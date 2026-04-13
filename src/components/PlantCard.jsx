36c7b758447fcc5328e2191042fa094ee057133f
abbi portwood <apwood09@gmail.com>
2026-04-13 12:43:26 -0400

    passed 10/10 test

--- src/components/PlantCard.jsx
+++ src/components/PlantCard.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ plant }) {
+  const { name, image, price } = plant; 
+
+  // state: in stock plant 
+  const [inStock, setIsStock] = useState(true);
+
+  // toggle function 
+  function handleToggleStock() {
+    setIsStock((prevStatus) => !prevStatus);
+  }
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
+      {inStock ? (
+        <button className="primary" onClick={handleToggleStock}>In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleToggleStock}>Out of Stock</button>
       )}
     </li>
   );
